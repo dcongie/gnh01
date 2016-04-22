2907959d840c0fa9954924cdffb9b8042e739422
Donald Ongyanco <donald.ongyanco@gmail.com>
2016-04-22 20:56:34 +0800

    2016/04/22

--- Main.py
+++ Main.py
@@ -5,11 +5,54 @@ import tkinter as tk            #Import Tkinter
 import time
 
 class App():
+    #const
+    MODBUS_MODE = 0x00
+    MODBUS_ALARM = 0x23
     CONST_CMV_MODE = 0
     CONST_AC_MODE = 1
     CONST_SIMV_MODE = 2
+    CONST_ALARM_HIGH_PRESSURE = 0x01
+    CONST_ALARM_LOW_PRESSURE = 0x02
+    CONST_ALARM_LOW_BATTERY = 0x04
+    CONST_ALARM_LOW_OXYGEN = 0x08
+    CONST_ALARM_HIGH_FREQ = 0x10
+
     timer1 = 0
+
+    #always updated
     Mode = 0
+    pressure = 0
+    ftol = 0
+    alarm = 0
+    battery = 0
+
+    #CMV
+    cmv_tidal_volume = 0
+    cmv_resp_rate = 0
+    cmv_fio2 = 0
+    cmv_high_pressure_limit = 0
+    cmv_high_frequency_limit = 0
+    cmv_low_pressure_limit = 0
+
+    #AC
+    ac_tidal_volume = 0
+    ac_trigger_pressure = 0
+    ac_pmax = 0
+    ac_resp_rate = 0
+    ac_fio2 = 0
+    ac_high_pressure_limit = 0
+    ac_high_frequency_limit = 0
+    ac_low_pressure_limit = 0
+
+    #SIMV
+    simv_tidal_volume = 0
+    simv_trigger_pressure = 0
+    simv_pmax = 0
+    simv_resp_rate = 0
+    simv_fio2 = 0
+    simv_high_pressure_limit = 0
+    simv_high_frequency_limit = 0
+    simv_low_pressure_limit = 0
 
     x = 1
     instrument = minimalmodbus.Instrument('COM5', 1, minimalmodbus.MODE_RTU)  # port name, slave address (in decimal)
@@ -18,16 +61,26 @@ class App():
     def __init__(self):
         self.root = tk.Tk()                         #Bind Tkinter to the root object
         #self.root.attributes('-fullscreen', True)
-        self.l_mode = tk.Label(text="1")
+        self.l_mode = tk.Label(text="")
         self.l_mode.pack()
+        self.l_alarm = tk.Label(text="")
+        self.l_alarm.pack()
+        self.l_pressure = tk.Label(text="")
+        self.l_pressure.pack()
+        self.l_ftol = tk.Label(text="")
+        self.l_ftol.pack()
+
         self.label = tk.Label(text="")
         self.label.pack()
+
         self.button_cmv = tk.Button(text="CMV", command=self.enter_cmv_mode)
         self.button_cmv.pack()
         self.button_ac = tk.Button(text="AC", command=self.enter_ac_mode)
         self.button_ac.pack()
         self.button_simv = tk.Button(text="SIMV", command=self.enter_simv_mode)
         self.button_simv.pack()
+        self.button_clear = tk.Button(text="Clear Alarm", command=self.clear_alarm)
+        self.button_clear.pack()
 
         #self.button2 = tk.Button(text="Go to Page Two", command=self.button2)
 
@@ -44,11 +97,53 @@ class App():
         self.instrument.serial.timeout = 0.05  # seconds
         time.sleep(2)
 
+    def read_cmv(self):
+        read_cmv = self.instrument.read_registers(0x01, 7)
+        print("[01h] %s" % read_cmv)
+
+
+#    def read_ac(self):
+
+#    def read_simv(self):
+
     def read_parameters(self):
         #read mode
-        read_mode = self.instrument.read_registers(0, 1)
-        print("[0] %s" % read_mode)
+        read_mode = self.instrument.read_registers(self.MODBUS_MODE, 1)
+        print("[00h] %s" % read_mode)
         self.Mode = read_mode[0]
+        if (self.Mode == self.CONST_CMV_MODE):
+            self.l_mode.config(text="CMV Mode")
+            self.read_cmv()
+        elif (self.Mode == self.CONST_AC_MODE):
+            self.l_mode.config(text="AC Mode")
+         #   read_ac()
+        elif (self.Mode == self.CONST_SIMV_MODE):
+            self.l_mode.config(text="SIMV Mode")
+        #    read_simv()
+
+    def read_regular(self):
+        # read_always
+        read_always = self.instrument.read_registers(0x20, 4)
+        print("[20h] %s" % read_always)
+        self.pressure = read_always[0] - 20
+        self.ftol = read_always[1]
+        self.alarm = read_always[3]
+
+    def update_alarm(self):
+        res = ""
+        if( (self.alarm & self.CONST_ALARM_HIGH_PRESSURE) == self.CONST_ALARM_HIGH_PRESSURE):
+            res += "High Airway Pressure Limit! "
+        if( (self.alarm & self.CONST_ALARM_LOW_PRESSURE) == self.CONST_ALARM_LOW_PRESSURE):
+            res += "Low Airway Pressure Limit! "
+        if( (self.alarm & self.CONST_ALARM_LOW_BATTERY) == self.CONST_ALARM_LOW_BATTERY):
+            res += "Low Battery! "
+        if( (self.alarm & self.CONST_ALARM_LOW_OXYGEN) == self.CONST_ALARM_LOW_OXYGEN):
+            res += "Low Oxygen! "
+        if( (self.alarm & self.CONST_ALARM_HIGH_FREQ) == self.CONST_ALARM_HIGH_FREQ):
+            res += "High Frequency! "
+        self.l_alarm.config(text=res)
+
+
 
     def update_clock(self):
         self.timer1 = self.timer1 + 1
@@ -56,14 +151,11 @@ class App():
             self.timer1 = 0
             self.read_parameters()
 
+        self.read_regular()
+        self.update_alarm()
 
 
-        if (self.Mode == self.CONST_CMV_MODE):
-            self.l_mode.config(text="CMV Mode")
-        elif (self.Mode == self.CONST_AC_MODE):
-            self.l_mode.config(text="AC Mode")
-        elif (self.Mode == self.CONST_SIMV_MODE):
-            self.l_mode.config(text="SIMV Mode")
+
 
         now = time.time()
         # strftime("%H:%M:%S")
@@ -72,20 +164,16 @@ class App():
         self.root.after(100, self.update_clock)
 
     def enter_cmv_mode(self):
-        self.instrument.write_register(0, self.CONST_CMV_MODE)
+        self.instrument.write_register(self.MODBUS_MODE, self.CONST_CMV_MODE)
 
     def enter_ac_mode(self):
-        self.instrument.write_register(0, self.CONST_AC_MODE)
+        self.instrument.write_register(self.MODBUS_MODE, self.CONST_AC_MODE)
 
     def enter_simv_mode(self):
-        self.instrument.write_register(0, self.CONST_SIMV_MODE)
+        self.instrument.write_register(self.MODBUS_MODE, self.CONST_SIMV_MODE)
 
-    def write_slogan(self):
-        #self.instrument.write_register(0, 0)
-        temperature = self.instrument.read_registers(0, 5)  # Registernumber, number of decimals
-        print(temperature)
-
-
+    def clear_alarm(self):
+        self.instrument.write_register(self.MODBUS_ALARM, 0)
 
 if __name__ == "__main__":
     app = App()
